78d5dce1653d1c29e0d880e08bb79361256c772b
xentixar <xentixar@gmail.com>
2025-09-02 22:57:49 +0545

    Enhance PushNotificationForm with scheduling features and user feedback
    - Added a live helper text to indicate the timezone for scheduled notifications.
    - Introduced a TextEntry component to display the time difference until the notification is sent, enhancing user awareness of scheduling.
    - Updated the visibility of the scheduled time difference based on user input, improving the form's interactivity.

--- src/Resources/PushNotifications/Schemas/PushNotificationForm.php
+++ src/Resources/PushNotifications/Schemas/PushNotificationForm.php
@@ -9,6 +9,8 @@ use Filament\Forms\Components\DateTimePicker;
 use Filament\Schemas\Schema;
 use Xentixar\FilamentPushNotifications\Enums\PushNotificationType;
 use App\Models\User;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Utilities\Get;
 
 class PushNotificationForm
 {
@@ -40,7 +42,13 @@ class PushNotificationForm
                 DateTimePicker::make('scheduled_at')
                     ->minDate(now())
                     ->label('Scheduled At')
-                    ->helperText('Leave empty to send immediately')
+                    ->live()
+                    ->helperText('Leave empty to send immediately. Timezone is ' . config('app.timezone') . '.')
+                    ->columnSpanFull(),
+                TextEntry::make('scheduled_at_difference')
+                    ->state(fn(Get $get): string => $get('scheduled_at') ? 'The notification will be sent in ' . (int) now()->diffInSeconds($get('scheduled_at')) . ' seconds.' : '')
+                    ->label('Scheduled At Difference')
+                    ->visible(fn(Get $get) => $get('scheduled_at'))
                     ->columnSpanFull(),
             ]);
     }
